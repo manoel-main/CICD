f02b7b2feec900b59761b012dbe51551f732757c
Hsiao <whsiao@deloitte.com>
2021-11-10 13:52:50 -0800

    fixed generator bug

--- public/script.js
+++ public/script.js
@@ -2,8 +2,17 @@ $("#generate").click(function(){
 	var lorem = $("#lorem");
 	lorem.html("");
 	var quantity = $("#quantity")[0].valueAsNumber;
-	var data = ["Lorem ipsum", "quia dolor sit", "amet", "consectetur"];
+	var data = ["Lorem ipsum <br> quia dolor sit <br> amet consectetur", 
+	"consectetur adipiscing <br> elit", 
+	"sed do eiusmod <br> tempor incididunt ut <br> labore et dolore <br> magna aliqua.", 
+	"Ut enim ad <br> minim veniam", 
+	"quis nostrud <br> exercitation <br> ullamco laboris", 
+	"nisi ut aliquip <br> ex ea commodo <br> consequat", 
+	"Duis aute irure <br> dolor in reprehenderit <br> in voluptate", 
+	"velit esse <br> cillum dolore <br> eu fugiat <br> nulla pariatur.", 
+	"Excepteur sint <br> occaecat cupidatat <br> non proident", 
+	"sunt in culpa <br> qui officia deserunt <br> mollit anim id <br> est laborum."];
 	for(var i = 0; i < quantity; i++){
-		lorem.append("<p>"+data[i]+"</p>");
+		lorem.append("<p>"+data[i % (data.length)]+"</p>"); 
 	}
 })
