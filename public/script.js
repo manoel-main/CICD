9f578df7bc57c598a86d389e3db63187d3847ec5
manoel-d <mtamraz@deloitte.com>
2021-11-10 15:04:01 -0800

    Update Copy Function

--- public/script.js
+++ public/script.js
@@ -1,9 +1,37 @@
+var text = "";
+
 $("#generate").click(function(){
 	var lorem = $("#lorem");
 	lorem.html("");
 	var quantity = $("#quantity")[0].valueAsNumber;
-	var data = ["Lorem ipsum", "quia dolor sit", "amet", "consectetur"];
+	var data = ["Lorem ipsum <br> quia dolor sit <br> amet consectetur", 
+	"consectetur adipiscing <br> elit", 
+	"sed do eiusmod <br> tempor incididunt ut <br> labore et dolore <br> magna aliqua.", 
+	"Ut enim ad <br> minim veniam", 
+	"quis nostrud <br> exercitation <br> ullamco laboris", 
+	"nisi ut aliquip <br> ex ea commodo <br> consequat", 
+	"Duis aute irure <br> dolor in reprehenderit <br> in voluptate", 
+	"velit esse <br> cillum dolore <br> eu fugiat <br> nulla pariatur.", 
+	"Excepteur sint <br> occaecat cupidatat <br> non proident", 
+	"sunt in culpa <br> qui officia deserunt <br> mollit anim id <br> est laborum."];
 	for(var i = 0; i < quantity; i++){
-		lorem.append("<p>"+data[i]+"</p>");
+		lorem.append("<p>"+data[i % (data.length)]+"</p>");
 	}
+	for(var i = 0; i < quantity; i++){
+		text += data[i % (data.length)];
+	}
+
+})
+
+$("#copy").click(function() {
+	/* Get the text field */
+	var copyText = text
+  
+
+  
+	 /* Copy the text inside the text field */
+	navigator.clipboard.writeText(copyText);
+  
+	/* Alert the copied text */
+	alert("Copied the text: " + copyText);
 })
